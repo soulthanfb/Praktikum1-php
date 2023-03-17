a60bc8607fd0bdb5f41e8ab55faefcc7d67f373d
soulthanti04 <fadhlillahbagussoulthan@gmail.com>
2023-03-17 17:50:55 +0700

    Praktikum PHP menggunakan Start Boostrap

---
+++ web/admin/daftar.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+?>
+<div>
+    <div class="container-fluid px-4">
+        <h1>Silakan Isi From Pendaftaran</h1>
+        <?php
+        // Sertakan form pendaftaran disini
+        // include_once 'form_daftar.php'
+        ?>
+    </div>
+</div>
+<?php
+include_once 'bottom.php';
+?>
