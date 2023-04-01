76b528c4f60f67953021e69a96928cd420838d6f
soulthanti04 <fadhlillahbagussoulthan@gmail.com>
2023-04-01 23:44:28 +0700

    pekan5

--- pekan5/databasephp-main/view_pelanggan.php
+++ pekan5/databasephp-main/view_pelanggan.php
@@ -17,11 +17,11 @@ require_once 'dbkoneksi.php';
     <tbody>
         <tr><td>ID</td><td><?=$row['id']?></td></tr>
         <tr>  <td>Kode</td><td><?=$row['kode']?></td></tr>
-        <tr>   <td>Nama Produk</td><td><?=$row['nama']?></td></tr>
-        <tr>   <td>Harga Jual</td><td><?=$row['harga_jual']?></td></tr>
-        <tr>   <td>Harga Beli</td><td><?=$row['harga_beli']?></td></tr>
-        <tr>   <td>Stok</td><td><?=$row['stok']?></td></tr>
-        <tr>   <td>Minimum Stok</td><td><?=$row['min_stok']?></td></tr>
-        <tr>   <td>Jenis Produk</td><td><?=$row['jenis_produk_id']?></td></tr>
+        <tr>   <td>Nama Pelanggan</td><td><?=$row['nama']?></td></tr>
+        <tr>   <td>Jenis Kelamin</td><td><?=$row['jk']?></td></tr>
+        <tr>   <td>Tempat Lahir</td><td><?=$row['tmp_lahir']?></td></tr>
+        <tr>   <td>Tanggal Lahir</td><td><?=$row['tgl_lahir']?></td></tr>
+        <tr>   <td>Email</td><td><?=$row['email']?></td></tr>
+        <tr>   <td>Kartu</td><td><?=$row['kartu_id']?></td></tr>
     </tbody>
 </table>
